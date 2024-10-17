46af40c5f5c343971d8e45ce1a436bb4039c5810
HusseinYusuf <youssoufhussein53@gmail.com>
2024-10-17 15:54:15 +0300

    fragments

--- app/src/main/java/com/example/online_shopping/MainActivity.java
+++ app/src/main/java/com/example/online_shopping/MainActivity.java
@@ -41,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.myBooking, R.id.about, R.id.contact, R.id.profile, R.id.termCondition, R.id.feedback, R.id.version)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
